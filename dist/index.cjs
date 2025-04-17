8c0903949c78900be6bc5d1fde0d0a7350ed63eb
melwinphilip <melwinphilip@gmail.com>
2025-04-17 11:47:10 +0300

    Update index.cjs

--- dist/index.cjs
+++ dist/index.cjs
@@ -35,7 +35,8 @@ class HANADBCheckpointSaver extends langgraph_checkpoint_1.BaseCheckpointSaver {
             value: "CHECKPOINT_WRITES"
         });
         this.client = client;
-        this.db = this.client.connect(dbName);
+        //this.db = this.client.connect(dbName);
+        this.db = client;
         this.checkpointTableName =
             checkpointTableName ?? this.checkpointTableName;
         this.checkpointWritesTableName =
